1fa8f380b203f3f7b6fe21bbc63597bbdf25005d
dyopa <aamood0821@gmail.com>
2024-05-21 11:51:43 +0900

    논리연산자

--- operator.js
+++ operator.js
@@ -2,5 +2,28 @@
 let a = 1;
 let b = 2;
 
-const c = (a++, b++);
+let c = (a++, b++);
+// const c = ((a = a + 1), (b = b + 1));
 console.log('c =', c, a, b);
+
+let d = (a--, b + a);
+console.log('d =', d, a, b);
+
+d = void (c = a + b);
+console.log(a, b, c, d);
+
+console.log('-------------------')
+const first = 'Uncle';
+const last = 'Bob';
+if (first) {
+    console.log(first);
+}
+console.log(last);
+
+let FullName = first ? first + ' ' : '';
+//          'Uncle ' + 'Bob'
+//            ''     + 'Bob'
+FullName = FullName + last;
+console.log('fullName =', FullName);
+
+console.log(`${first}${first ? ' ' : ''}${last}`);
