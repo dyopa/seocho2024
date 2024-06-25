ec7221d8cdf5e977ff4123d438443060b7d871cd
dyopa <aamood0821@gmail.com>
2024-06-25 11:47:28 +0900

    usememo

--- react/rbvite/src/components/My.jsx
+++ react/rbvite/src/components/My.jsx
@@ -1,75 +1,31 @@
-import { useEffect, useLayoutEffect, useRef, useState } from "react";
-import {
-  FaCartPlus,
-  FaUndoAlt,
-  FaTrashAlt,
-  FaEdit,
-  FaSave,
-} from "react-icons/fa";
+import { useEffect, useLayoutEffect, useMemo, useState } from "react";
+import { FaTrashAlt, FaEdit } from "react-icons/fa";
 import Login from "./Login";
 import Profile from "./Profile";
 import Button from "./atoms/Button";
 import SampleAtoms from "./atoms/SampleAtoms";
-import Input from "./atoms/Input";
+import ItemEdit from "./ItemEdit";
 
 export default function My({
   session: { loginUser, cart },
   signOut,
   signIn,
   removeItem,
-  addItem: addCartItem,
-  saveItem: saveCartItem,
+  addItem,
+  saveItem,
 }) {
   const [isAdding, setIsAdding] = useState(false);
   const [editingItem, setEditingItem] = useState(null);
-  const itemNameRef = useRef();
-  const itemPriceRef = useRef();
 
-  const cancelAdding = (evt) => {
-    evt.preventDefault();
-    setIsAdding(false);
-  };
-
-  const addItem = (evt) => {
-    evt.preventDefault();
-    const itemName = itemNameRef.current.value;
-    const itemPrice = itemPriceRef.current.value;
-    console.log("🚀  itemName, itemPrice:", itemName, itemPrice);
-    if (!itemName || !itemPrice) {
-      alert("상품명과 금액을 정확히 입력하세요!");
-      itemNameRef.current.focus();
-      return;
-    }
-    addCartItem(itemName, +itemPrice);
+  const cancelAdding = () => {
     setIsAdding(false);
   };
 
   const editing = (itemId) => {
     setEditingItem(cart.find((item) => item.id === itemId));
   };
-  useEffect(() => {
-    if (editingItem) {
-      itemNameRef.current.value = editingItem.name;
-      itemPriceRef.current.value = editingItem.price;
-      itemNameRef.current.select();
-    }
-  }, [editingItem]);
 
   const cancelEditing = () => {
-    setEditingItem(null);
-  };
-
-  const saveItem = (evt) => {
-    evt.preventDefault();
-    const itemName = itemNameRef.current.value;
-    const itemPrice = itemPriceRef.current.value;
-    console.log("🚀  itemName, itemPrice:", itemName, itemPrice);
-    if (!itemName || !itemPrice) {
-      alert("상품명과 금액을 정확히 입력하세요!");
-      itemNameRef.current.focus();
-      return;
-    }
-    saveCartItem(editingItem.id, itemName, +itemPrice);
     setEditingItem(null);
   };
 
@@ -77,6 +33,7 @@ export default function My({
   const [time, setTime] = useState(
     Math.round(new Date().getTime() / 1000) % 1000,
   );
+
   useEffect(() => {
     const intl = setInterval(() => {
       // console.log("time=", time);
@@ -84,29 +41,38 @@ export default function My({
     }, 1000);
 
     return () => {
-      console.log("🚀  intl clear!!");
+      // console.log("🚀  intl clear!!");
       clearInterval(intl);
     };
   }, []);
 
-  const [posts, setPosts] = useState([]);
-  useEffect(() => {
-    const controller = new AbortController();
-    const { signal } = controller;
-    console.log("fetch!!!");
-    fetch("https://jsonplaceholder.typicode.com/posts?userId=2", signal)
-      .then((res) => res.json())
-      .then((data) => setPosts(data));
+  // const [posts, setPosts] = useState([]);
+  // useEffect(() => {
+  //   const controller = new AbortController();
+  //   const { signal } = controller;
+  //   // console.log("fetch!!!");
+  //   fetch("https://jsonplaceholder.typicode.com/posts?userId=2", signal)
+  //     .then((res) => res.json())
+  //     .then((data) => setPosts(data));
 
-    return () => {
-      console.log("abort!!");
-      controller.abort();
-    };
-  }, []);
+  //   return () => {
+  //     // console.log("abort!!");
+  //     controller.abort();
+  //   };
+  // }, []);
 
   useLayoutEffect(() => {
-    console.log("useLayoutEffect!!!!!!");
+    // console.debug("useLayoutEffect!!!!!!");
   }, []);
+
+  const addingItem = useMemo(() => ({ name: "", price: 1000 }), []);
+
+  const totalPrice = useMemo(() => {
+    // console.log("ttttttttttttttt");
+    return cart?.reduce((acc, item) => acc + item.price, 0);
+  }, [cart]);
+
+  // const MemoedItemEdit = memo(ItemEdit);
 
   return (
     <>
@@ -116,9 +82,7 @@ export default function My({
         <Login singIn={signIn} />
       )}
 
-      <h1>
-        Second: {time} - posts: {posts.length}
-      </h1>
+      <h1>Second: {time}</h1>
 
       <div className="my-5 border text-center">
         <ul>
@@ -126,25 +90,11 @@ export default function My({
             ? cart.map((item) => (
                 <li key={item.id} className="flex justify-between border-b">
                   {editingItem?.id === item.id ? (
-                    <form className="m-2 flex gap-3 border border-green-300 p-3">
-                      <Input ref={itemNameRef} placeholder="상품명" />
-                      <Input
-                        ref={itemPriceRef}
-                        type="number"
-                        placeholder="금액"
-                      />
-                      <Button
-                        text={<FaUndoAlt />}
-                        onClick={cancelEditing}
-                        size="sm"
-                      />
-                      <Button
-                        text={<FaSave />}
-                        onClick={saveItem}
-                        type="primary"
-                        size="sm"
-                      />
-                    </form>
+                    <ItemEdit
+                      item={editingItem}
+                      cancel={cancelEditing}
+                      save={saveItem}
+                    />
                   ) : (
                     <>
                       <span className="text-xs text-gray-300">{item.id}</span>
@@ -163,7 +113,9 @@ export default function My({
                           className="py-1"
                         />
                         <Button
-                          onClick={() => removeItem(item.id)}
+                          onClick={() => {
+                            if (confirm("Are u sure??")) removeItem(item.id);
+                          }}
                           type="danger"
                           text={<FaTrashAlt />}
                           size="xs"
@@ -176,19 +128,13 @@ export default function My({
               ))
             : "장바구니가 비었습니다."}
         </ul>
+        <h3 className="pl-1 text-left text-green-500">
+          * Total: {totalPrice.toLocaleString()}원
+        </h3>
         {isAdding ? (
-          <form className="m-2 flex gap-3 border border-green-300 p-3">
-            <Input ref={itemNameRef} placeholder="상품명" />
-            <Input ref={itemPriceRef} type="number" placeholder="금액" />
-            <Button text={<FaUndoAlt />} onClick={cancelAdding} size="sm" />
-            <Button
-              text={<FaCartPlus />}
-              onClick={addItem}
-              type="primary"
-              size="sm"
-            />
-          </form>
+          <ItemEdit item={addingItem} cancel={cancelAdding} save={addItem} />
         ) : (
+          // <MemoedItemEdit />
           <Button
             onClick={() => setIsAdding(true)}
             text="+ 상품추가"
@@ -196,6 +142,8 @@ export default function My({
           />
         )}
       </div>
+
+      {/* <MemoedItemEdit /> */}
 
       <SampleAtoms />
     </>
