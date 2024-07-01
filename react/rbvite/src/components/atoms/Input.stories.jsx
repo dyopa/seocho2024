038c70e018c11b7de2b05ebe414af42277b0b539
dyopa <aamood0821@gmail.com>
2024-07-01 16:51:15 +0900

    next

---
+++ react/rbvite/src/components/atoms/Input.stories.jsx
@@ -0,0 +1,15 @@
+import Input from "./Input";
+
+const meta = {
+  component: Input,
+  title: "Atomic Input",
+  tags: ["autodocs"],
+};
+
+export const DefaultInput = {
+  args: {
+    label: "DefaultInput",
+  },
+};
+
+export default meta;
